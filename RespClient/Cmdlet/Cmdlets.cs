fcd67afba371931960abf4eaf863c72942f298de
guitarrapc <cherishacquire@outlook.com>
2014-09-02 23:50:28 +0900

    add comment. Add Get-RedisInfo Cmdlet to retrieve info result as Dictionary Object.

--- RespClient/Cmdlet/Cmdlets.cs
+++ RespClient/Cmdlet/Cmdlets.cs
@@ -1,10 +1,18 @@
 ﻿using Redis.Protocol;
 using System;
+using System.Linq;
 using System.Management.Automation;
 using System.Text;
 
 namespace Redis.PowerShell.Cmdlet
 {
+    /// <summary>
+    /// Open socket to connect specified Redis Server.
+    /// If connection already establishded to some host, then that connection will dispose and new connection is created.
+    /// </summary>
+    /// <param name="Host">Redis server computer name or IPAddress. default is 127.0.0.1</param>
+    /// <param name="Port">Redis Server port number waiting connection. default is 6379</param>
+    /// <param name="IoTimeout">Socket client timeout values. default is -1</param>
     [Cmdlet(VerbsCommunications.Connect, "RedisServer")]
     public class ConnectRedisServer : System.Management.Automation.Cmdlet
     {
@@ -21,6 +29,7 @@ namespace Redis.PowerShell.Cmdlet
 
         protected override void BeginProcessing()
         {
+            // existing connection will be disposed.
             if (Global.RespClient != null) Global.RespClient.Dispose();
 
             var client = new RespClient(Host ?? "127.0.0.1", Port ?? 6379, IoTimeout ?? -1);
@@ -30,6 +39,10 @@ namespace Redis.PowerShell.Cmdlet
         }
     }
 
+    /// <summary>
+    /// Get current socket Redis server connection info.
+    /// You can see host, port and ioTimeout.
+    /// </summary>
     [Cmdlet(VerbsCommon.Get, "RedisCurrentInfo")]
     public class GetRedisCurrentInfo : System.Management.Automation.Cmdlet
     {
@@ -42,6 +55,9 @@ namespace Redis.PowerShell.Cmdlet
         }
     }
 
+    /// <summary>
+    /// Disconnect current Redis server socket connection.
+    /// </summary>
     [Cmdlet(VerbsCommunications.Disconnect, "RedisServer")]
     public class DisconnectRedisServer : System.Management.Automation.Cmdlet
     {
@@ -55,6 +71,12 @@ namespace Redis.PowerShell.Cmdlet
         }
     }
 
+    /// <summary>
+    /// Send Command to the Redis server.
+    /// Command will be immediately executed.
+    /// Pipeline will queue command if there are already pipeline exist..
+    /// </summary>
+    /// <param name="Command">Redis Command to send. e.g. info</param>
     [Cmdlet(VerbsCommunications.Send, "RedisCommand")]
     public class SendCommand : System.Management.Automation.Cmdlet
     {
@@ -78,6 +100,11 @@ namespace Redis.PowerShell.Cmdlet
         }
     }
 
+    /// <summary>
+    /// Send Command to the Redis server with pipeline mode.
+    /// You don't need to handle pipeline status with this mode.
+    /// </summary>
+    /// <param name="Command">Redis Commands to send. e.g. "info", "config get save"</param>
     [Cmdlet(VerbsCommunications.Send, "RedisPipelineCommand")]
     public class PipelineCommand : System.Management.Automation.Cmdlet
     {
@@ -104,6 +131,10 @@ namespace Redis.PowerShell.Cmdlet
         }
     }
 
+    /// <summary>
+    /// Begin pipeline.
+    /// Command will be queued into pipeline from this cmdlet.
+    /// </summary>
     [Cmdlet("Begin", "RedisPipeline")]
     public class BeginPipeline : System.Management.Automation.Cmdlet
     {
@@ -116,6 +147,10 @@ namespace Redis.PowerShell.Cmdlet
         }
     }
 
+    /// <summary>
+    /// Execute queued command in pipeline.
+    /// Make sure you have created pipeline.
+    /// </summary>
     [Cmdlet("Execute", "RedisPipeline")]
     public class ExecutePipeline : System.Management.Automation.Cmdlet
     {
@@ -132,6 +167,38 @@ namespace Redis.PowerShell.Cmdlet
             {
                 Global.PipelineCommand = null;
             }
+        }
+    }
+
+    /// <summary>
+    /// Get Redis info with info command.
+    /// </summary>
+    /// <param name="All">change "info" to "info all".</param>
+    [Cmdlet(VerbsCommon.Get, "RedisInfo")]
+    public class GetRedisInfoCommand : System.Management.Automation.Cmdlet
+    {
+        [Parameter(ParameterSetName = "All", Position = 0, Mandatory = false, ValueFromPipeline = true, ValueFromPipelineByPropertyName = true)]
+        public SwitchParameter All { get; set; }
+
+        private string Command = "info";
+
+        protected override void BeginProcessing()
+        {
+            if (Global.RespClient == null) throw new InvalidOperationException("Server is not connecting");
+
+            // use "info all"
+            if (All == true) { Command = Command + " " + "all"; }
+        }
+
+        protected override void EndProcessing()
+        {            
+            // no pipeline mode
+            var value = Global.RespClient.SendCommand(Command, x => Encoding.UTF8.GetString(x));
+
+            // parse string to List
+            var InfoCommand = new Redis.Protocol.RespClient.InfoCommand();
+            var dicationary = InfoCommand.ParseInfo(value);
+            foreach (var x in dicationary) { this.WriteObject(x); }
         }
     }
 }
